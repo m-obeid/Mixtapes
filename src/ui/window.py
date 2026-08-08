4e4293212d99307b91c95417906d8a97ab240362
Ryan Wu <ryanwusd@gmail.com>
2026-08-08 13:16:49 -0700

    fix: search bar inconsistency

--- src/ui/window.py
+++ src/ui/window.py
@@ -386,7 +386,6 @@ class MainWindow(Adw.ApplicationWindow):
         self.root_content_view.add_top_bar(self.header_bar)
 
         self.search_bar = Gtk.SearchBar()
-        self.search_bar.set_key_capture_widget(self)  # Capture keys
         self.search_bar.connect(
             "notify::search-mode-enabled", self.on_search_mode_changed
         )
@@ -2656,16 +2655,13 @@ class MainWindow(Adw.ApplicationWindow):
                 if root_page and nav.get_previous_page(root_page):
                     nav.pop_to_tag("root")
 
-            # Manually trigger search mode and insert the character
-            # This avoids the "ignored first character" bug during tab switches
-            self.search_bar.set_search_mode(True)
-            self.search_entry.grab_focus()
-            self.search_entry.set_text(char)
-            self.search_entry.set_position(-1)  # Move cursor to end
-            return True
-
-        # Let the event propagate so GtkSearchBar can capture it
-        return False
+        # Manually trigger search mode and insert the character
+        # This avoids the "ignored first character" bug during tab switches
+        self.search_bar.set_search_mode(True)
+        self.search_entry.grab_focus()
+        self.search_entry.set_text(char)
+        self.search_entry.set_position(-1)  # Move cursor to end
+        return True
 
     def on_global_search_changed(self, entry):
         text = entry.get_text()
